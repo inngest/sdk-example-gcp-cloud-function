2d1efa70fdb7f02dda470d9ceffe3c3674343595
Darwin D Wu <darwin67@users.noreply.github.com>
2023-11-02 16:52:42 -0700

    add cron

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const functions = require("@google-cloud/functions-framework");
 const { Inngest } = require("inngest");
 const { serve } = require("inngest/express");
 
-const inngest = new Inngest({ id: "gcp-sample", name: "GCP CloudFunction Example" })
+const inngest = new Inngest({
+  id: "gcp-sample",
+  name: "GCP CloudFunction Example",
+  baseUrl: "https://stage.inn.gs"
+})
 
 const hello = inngest.createFunction(
   { id: "hello-world", name: "Hello World" },
   { event: "test/hello" },
-  async ({ step }) => {
+  async ({ step, event }) => {
+     await step.run("datetime", () => {
+      return event.data.datetime || null;
+    })
+
     const greet = await step.run("say-hello", () => {
       return "hello"
     })
@@ -16,10 +24,28 @@ const hello = inngest.createFunction(
   }
 )
 
+const minute = inngest.createFunction(
+  { id: "every-minute", name: "Every Minute" },
+  { cron: "* * * * *" },
+  async ({ step }) => {
+    const datetime = await step.run("current-datetime", () => Date.now())
+
+    await step.sleep("1m")
+
+    await step.sendEvent("hello", {
+      name: "test/hello",
+      data: { datetime: datetime }
+    })
+
+    return datetime
+  }
+)
+
 functions.http(
   "inngest",
   serve({
     client: inngest,
-    functions: [hello]
+    functions: [hello, minute],
+    baseUrl: "https://api.inngest.net"
   })
 )
